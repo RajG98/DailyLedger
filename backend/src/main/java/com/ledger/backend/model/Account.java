16b8687de6c16961550ee4a34f2ba4710967e4d1
RajG98 <rajarshig007@gmail.com>
2025-12-19 17:15:40 +0000

    update 18/12
    Signed-off-by: Rajarshi <rajarshig007@gmail.com>

--- backend/src/main/java/com/ledger/backend/model/Account.java
+++ backend/src/main/java/com/ledger/backend/model/Account.java
@@ -1,17 +1,13 @@
 package com.ledger.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.List;
-import java.util.UUID;
 
 @Entity
 @Data
@@ -25,12 +21,10 @@ public class Account {
     private String id;
     private String title;
     private String amount;
-
     @ManyToOne
     @JoinColumn(name = "user_id")
     @JsonBackReference
     private User user;
-
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private List<Transaction> transaction;
